09c0426151040a09e80c1b9b32c63c50f22cabb3
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2023-07-27 09:58:46 +0100

    Update shuffler.py

--- shuffler.py
+++ shuffler.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import random
-import time
 
 def standup_number_roulette():
     st.title("Standup Number Roulette")
@@ -8,20 +7,14 @@ def standup_number_roulette():
     team_members = st.text_area("Enter the names of team members (one name per line):")
     team_members_list = [name.strip() for name in team_members.split('\n') if name.strip()]
 
-    if st.button("Start Roulette"):
+    if st.button("Assign Numbers"):
         if not team_members_list:
             st.warning("Please enter the names of team members.")
         else:
-            st.write("Spinning the roulette wheel...")
-            st.write("And the number is...")
-
-            # Shuffle the list of team members repeatedly to create the spinning effect
-            for i in range(20):
-                random.shuffle(team_members_list)
-                st.write(f"{i+1}. {team_members_list[0]}")
-                time.sleep(0.5)  # Add a pause between each spin for animation effect
-
-            st.success("Number assigned: 1. " + team_members_list[0])
+            random.shuffle(team_members_list)
+            st.write("Here's the assigned order for standup presentation:")
+            for i, name in enumerate(team_members_list, start=1):
+                st.write(f"{i}. {name}")
 
 if __name__ == "__main__":
     standup_number_roulette()
